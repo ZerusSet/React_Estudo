a7ec9addbedba152901af9172ef65ee0317edeaa
João Pedro <joaopedro.llopes@gmail.com>
2020-03-05 17:18:06 -0300

    Modulo 04 - React

---
+++ Modulo 04 - React/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+
+import TechList from './components/TechList'
+// import profile from './assets/lb.jpg';
+
+function App(){
+    return <TechList/>
+}
+
+export default App;
